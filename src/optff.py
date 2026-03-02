c5a464e796dd6e5a7404f875f5e270ae6d45fa23
JetpackJackson <jetpackjackson@katzenmue.de>
2026-03-01 21:36:59 -0500

    add optff algo

--- src/optff.py
+++ src/optff.py
@@ -1,8 +1,42 @@
 import main
 
 
-def print_results():
-    print("OPTFF : <number_of_misses>")
+def optff(k, requests):
+    cache = set()
+    misses = 0
+
+    for i, r in enumerate(requests):
+        if r in cache:
+            # hit
+            pass
+        else:
+            # miss
+            misses += 1
+
+            if len(cache) < k:
+                # cache not full
+                cache.add(r)
+            else:
+                # cache full, eviction notice
+                future_use = {}
+                for page in cache:
+                    # find next use of page
+                    next_use = float("inf")
+                    for j in range(i + 1, len(requests)):
+                        if requests[j] == page:
+                            next_use = j
+                            break
+                    future_use[page] = next_use
+                # boot
+                to_evict = max(future_use, key=future_use.get)
+                cache.remove(to_evict)
+                cache.add(r)
+
+    return misses
+
+
+def print_results(k, r):
+    print(f"OPTFF : {optff(k,r)}")
 
 
 if __name__ == "__main__":
